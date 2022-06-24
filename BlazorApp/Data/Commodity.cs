de2bcc30ce3325df40ab1aecd310bb2f663b9738
LXX987 <2590168411@qq.com>
2022-06-24 17:23:14 +0800

    lxt

---
+++ BlazorApp/Data/Commodity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp.Data
+{
+    public class Commodity
+    {
+        public int commodity_id { get; set; }
+
+        public int user_id { get; set; }
+
+        public string commodity_name { get; set; } = null!;
+
+        public int item_price { get; set; }
+
+        public int total_count { get; set; }
+
+        public DateTime start_time { get; set; }
+
+        public DateTime end_time { get; set; }
+    }
+}
--- BlazorApp/Service/UserApiClient.cs
+++ BlazorApp/Service/UserApiClient.cs
@@ -1,5 +1,13 @@
 ﻿using System.Net.Http.Headers;
 using BlazorApp.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Linq;
 
 //写一个类型化的HttpClient，然后注册服务
 namespace BlazorApp.Service
@@ -18,6 +26,20 @@ namespace BlazorApp.Service
             var result = await _httpClient.GetFromJsonAsync<User[]>("/api/user/getall");
 
             return result;
+        }
+
+        public async Task<int> searchCommodityt(string commodity_name)
+        {
+            Console.WriteLine(commodity_name);
+            Commodity commodity = new Commodity();
+            commodity.commodity_name = commodity_name;
+            var result = await _httpClient.PostAsJsonAsync("/api/commodity/searchCommodity", commodity);
+            Console.WriteLine(result);
+            Console.WriteLine(result.Content.ReadFromJsonAsync<Commodity>());// 好像是没用的
+            var stringResponse = await result.Content.ReadAsStringAsync();// 靠这个
+            Console.WriteLine(stringResponse);
+            /*Console.WriteLine(JsonSerializer.Deserialize<Commodity>(stringResponse));*/
+            return 0;
         }
     }
 }
